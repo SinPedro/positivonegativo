a4beb9250665bca2240474d6f9b2041984d4444d
Pedro de Souza Martins <143435476+SinPedro@users.noreply.github.com>
2023-09-13 14:59:37 -0300

    Add files via upload

---
+++ positivonegativo/pn.js
@@ -0,0 +1,11 @@
+let numeroSinal = Number(prompt("Digite o número a ser verificado"));
+
+// impressão do resultado 
+
+if (numeroSinal >= "0"){
+    console.log("positivo");
+}
+
+else {
+    console.log("negativo");
+}
